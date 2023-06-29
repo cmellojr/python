4e7b63c563bef2586d3c24ef50fd2fa025439c05
Carlos Mello Jr <cmellojr@gmail.com>
2023-06-29 10:39:36 -0300

    Adding the shebang to several files.

---
+++ even_string.py
@@ -0,0 +1,14 @@
+def myfunc(s):
+    new_string = list(s)
+    for item in range(len(new_string)):
+        if item % 2 == 0:
+            new_string[item] = str(new_string[item]).upper()
+        else:
+            new_string[item] = str(new_string[item]).lower()
+    str_to_return = ''.join(new_string) 
+#    for x in new_string:
+#        str_to_return += x
+    return str_to_return
+
+teste = myfunc('Palmeiras')
+print(teste)
